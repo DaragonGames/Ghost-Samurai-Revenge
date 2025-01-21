1fd8f43deebf014d12b1abde792beeea3d52e795
Stefan Gerstner <stefan.gerstner96@web.de>
2025-01-21 12:45:34 +0100

    FMOD implementation

--- Assets/Scripts/Sound/SoundEffect.cs
+++ Assets/Scripts/Sound/SoundEffect.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class SoundEffect : MonoBehaviour
 {
-    public AudioClip[] audioClips;
-
     // Start is called before the first frame update
     void Start()
     {
-        AudioClip clip = audioClips[Random.Range(0, audioClips.Length)];
-        GetComponent<AudioSource>().clip = clip;
-        GetComponent<AudioSource>().Play();
-        GetComponent<AudioSource>().volume = AudioMaster.SFXVolume;
-        Destroy(gameObject, clip.length +0.1f);
+
+        Destroy(gameObject, 5);
     }
     
 }
