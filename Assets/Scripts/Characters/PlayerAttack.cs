7782964f10765f62822a0b1bbbc36a304593d8e7
Stefan Gerstner <stefan.gerstner96@web.de>
2025-01-25 20:49:21 +0100

    Adjusted Attacks for new Features

--- Assets/Scripts/Characters/PlayerAttack.cs
+++ Assets/Scripts/Characters/PlayerAttack.cs
@@ -9,53 +9,56 @@ public class PlayerAttack : ScriptableObject
     public float attackCost;
     public GameObject projectilePrefab;
     public GameObject soundPrefab;
-    public bool isSclice;
-
-    void CreateSlice(Transform transform, PlayerStats stats, Vector3 attackDirection)
-    {
-        Instantiate(soundPrefab, transform.position, Quaternion.identity); 
-        // Create the Projectile
-        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity, transform); 
-        projectile.GetComponent<Projectile>().SetValues(Vector3.zero, "Player", stats.attackSpeed*attackTime.y);
-
-        // Change Position & Rotation based on Facing Direction
-        float facing = Player.FacingFromVector(attackDirection);
-        switch (facing)
-        {
-            case 0:
-                projectile.transform.position += new Vector3(0, 0.8f,0);
-                projectile.transform.Rotate(new Vector3(0,0,90));
-                break;
-            case 1:
-                projectile.transform.position += new Vector3(0.65f, -0.1f,0);
-                break;
-            case 2:
-                projectile.transform.position +=new Vector3(0, -0.8f,0);
-                projectile.transform.Rotate(new Vector3(0,0,90));
-                break;
-            case 3:
-                projectile.transform.position += new Vector3(-0.65f, -0.1f,0);
-                break;
-        }
-    } 
-
-    void CreateProjectile(Transform transform, PlayerStats stats, Vector3 attackDirection) 
-    {
-        Instantiate(soundPrefab, transform.position, Quaternion.identity); 
-        Vector3 spawnPosition = transform.position + attackDirection.normalized;
-        GameObject projectile = Instantiate(projectilePrefab, spawnPosition, Quaternion.identity);        
-        projectile.GetComponent<Projectile>().SetValues(attackDirection.normalized, "Player", 10);       
-    }
+    public AttackType attackType;
+    public enum AttackType {thrown, closeCombat};
 
     public void Attack(Player player, Vector3 attackDirection)
-    {
-        if (isSclice)
-        {
-            CreateSlice(player.transform, player.GetStats(), attackDirection);
+    {    
+        // Figure out values based on type
+        float duration = 10;
+        Transform parent = null;
+        if (attackType == AttackType.closeCombat)
+        {   
+            duration = player.GetStats().attackSpeed*attackTime.y;
+            parent = player.transform;      
         }
-        else
+
+        // Create and adjust Projectille
+        GameObject projectile = Instantiate(projectilePrefab, player.transform.position, Quaternion.identity, parent); 
+        projectile.GetComponent<Projectile>().SetValues(attackDirection.normalized, "Player", duration);
+        AdjustPosition(projectile.transform, attackDirection);   
+
+        // Create a Soundeffect
+        Instantiate(soundPrefab,player.transform.position, Quaternion.identity); 
+    }
+
+    private void AdjustPosition(Transform projectile, Vector3 attackDirection)
+    {
+        switch (attackType)
         {
-            CreateProjectile(player.transform, player.GetStats(),attackDirection);
+            case AttackType.thrown:
+                projectile.transform.position += attackDirection.normalized;
+                break;
+            case AttackType.closeCombat:
+                float facing = Player.FacingFromVector(attackDirection);
+                switch (facing)
+                {
+                    case 0:
+                        projectile.transform.position += new Vector3(0, 0.8f,0);
+                        projectile.transform.Rotate(new Vector3(0,0,90));
+                        break;
+                    case 1:
+                        projectile.transform.position += new Vector3(0.65f, -0.1f,0);
+                        break;
+                    case 2:
+                        projectile.transform.position +=new Vector3(0, -0.8f,0);
+                        projectile.transform.Rotate(new Vector3(0,0,90));
+                        break;
+                    case 3:
+                        projectile.transform.position += new Vector3(-0.65f, -0.1f,0);
+                        break;
+                }
+                break;
         }
     }
 }
